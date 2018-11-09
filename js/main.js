9c9e466b701e37f3c6632f0f5d1cba1547a0e273
mofoomofoo <44883389+mofoomofoo@users.noreply.github.com>
2018-11-09 18:10:43 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 $(function(){
 
  $(window).scroll(function(){
-    if($(window).scrollTop() > $("#first").offset().top && $(window).scrollTop() < $("#second").offset().top ){
-        $(".about").addClass("bigger");
-        $(".works, .hobbies").removeClass("bigger");
-    } else if ($(window).scrollTop() > $("#second").offset().top && $(window).scrollTop() < $("#third").offset().top ){
-        $(".works").addClass("bigger");
-        $(".about, .hobbies").removeClass("bigger");
-    } else if ($(window).scrollTop() > $("#third").offset().top && $(window).scrollTop() < $("#forth").offset().top ){
-        $(".hobbies").addClass("bigger");
-        $(".works, .worlds").removeClass("bigger");
-    } else if ($(window).scrollTop() > $("#forth").offset().top && $(window).scrollTop() < $("#fifth").offset().top ){
-        $(".worlds").addClass("bigger");
-        $(".links, .hobbies").removeClass("bigger");
-    } else if ($(window).scrollTop() > $("#fifth").offset().top ){
-        $(".links").addClass("bigger");
-        $(".worlds").removeClass("bigger");
+    if($(window).scrollTop() >= $("#first").offset().top && $(window).scrollTop() < $("#second").offset().top ){
+        $(".navigator ul li").css("display","block");
+        $(".about").addClass("hide");
+        $(".nav-bar").addClass("block");
+        $(".works, .hobbies").removeClass("hide");
+    } else if ($(window).scrollTop() >= $("#second").offset().top && $(window).scrollTop() < $("#third").offset().top ){
+        $(".navigator ul li").css("display","block");
+        $(".works").addClass("hide");
+        $(".about, .hobbies").removeClass("hide");
+    } else if ($(window).scrollTop() >= $("#third").offset().top && $(window).scrollTop() < $("#forth").offset().top ){
+        $(".navigator ul li").css("display","block");
+        $(".hobbies").addClass("hide");
+        $(".works, .worlds").removeClass("hide");
+    } else if ($(window).scrollTop() >= $("#forth").offset().top && $(window).scrollTop() < $("#fifth").offset().top ){
+        $(".navigator ul li").css("display","block");
+        $(".worlds").addClass("hide");
+        $(".hobbies, .links").removeClass("hide");
+    } else if ($(window).scrollTop() >= $("#fifth").offset().top ){
+        $(".navigator ul li").css("display","block");
+        $(".links").addClass("hide");
+        $(".worlds").removeClass("hide");
     } else {
-        $(".about, .works, .hobbies").removeClass("bigger");
+        $(".navigator ul li").css("display","inline-block");
+        $(".about, .works, .hobbies").removeClass("hide");
     }
 
 
